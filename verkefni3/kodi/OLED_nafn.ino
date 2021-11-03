ca66fcc35d377ac96a8e9ea7ccc1ff9442cbcafa
Snorri Már Gunnarsson <70167747+snorrimar4@users.noreply.github.com>
2021-11-03 09:09:50 +0000

    Create OLED_nafn.ino

---
+++ verkefni3/kodi/OLED_nafn.ino
@@ -0,0 +1,8 @@
+// Display Text
+display.clearDisplay();
+display.setTextSize(1);
+display.setTextColor(WHITE);
+display.setCursor(0,28);
+display.println("Snorri Mar");
+display.display();
+delay(2000);
